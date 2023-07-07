51d6ac06e28ac0800c54ed02df702a67564fe393
itsmevikas1009 <chaurasiavikas100904@gmail.com>
2023-07-07 07:40:25 +0530

    Hello World!

---
+++ JavaBasics.java
@@ -0,0 +1,16 @@
+//Boilerplate Code
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\n");
+        System.out.println("* * * * ");
+        System.out.println("* * * ");
+        System.out.println("* * ");
+        System.out.println("* ");
+        int a = 10;
+        int b = 5;
+        System.out.println(a +" + "+ b +" = "+ (a+b));
+        String name = "Akash Chopra";
+        System.out.println(name);
+    }
+}
