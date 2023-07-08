ea67553e167aecd42bc45981a148f0d69e915012
itsmevikas1009 <chaurasiavikas100904@gmail.com>
2023-07-08 07:42:11 +0530

    Type Conversion

---
+++ TypeConversion_Cast.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class TypeConversion_Cast {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        float b = a;
+        System.out.println(b);
+        int c = 200;
+        long d= c;
+        System.out.println(d);
+        float e = sc.nextInt();
+        System.out.println(e);
+    }
+}
