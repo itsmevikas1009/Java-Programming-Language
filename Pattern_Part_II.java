ac5021c1ddc1ddd863382365db2deb77f8c98a94
itsmevikas1009 <chaurasiavikas100904@gmail.com>
2023-07-26 10:54:58 +0530

    Pattern Part - II

--- Pattern_Part_II.java
+++ Pattern_Part_II.java
@@ -27,6 +27,16 @@ public class Pattern_Part_II {
             System.out.println();
         }
     }
+
+    //Function to Inverted Half-Pyramid with Numbers
+    public static void half_Pyramid_Num(int num) {
+        for(int i = 1; i <= num; i++) {
+            for(int j = 1; j <= num - i + 1; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -45,6 +55,11 @@ public class Pattern_Part_II {
         int n = sc.nextInt();
         half_rotated_Pyramid(n);
         */
+
+        //Function call for Print Inverted Half-Pyramid with Numbers
+        System.out.print("Enter Number of Row: ");
+        int num = sc.nextInt();
+        half_Pyramid_Num(num);
 
 
     }
